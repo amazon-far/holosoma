a61a88a32cca9487116e0b2a4f6d53171d1cab1c
Sajedeh99 <ssajedehtaheri@gmail.com>
2026-08-07 18:19:24 +0330

    customize holosoma setups for ILIA

--- scripts/setup_isaacgym.sh
+++ scripts/setup_isaacgym.sh
@@ -22,8 +22,8 @@ if [[ ! -f $SENTINEL_FILE ]]; then
 
   # Create the conda environment
   if [[ ! -d $ENV_ROOT ]]; then
-    $CONDA_ROOT/bin/conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/main
-    $CONDA_ROOT/bin/conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/r
+    # $CONDA_ROOT/bin/conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/main
+    # $CONDA_ROOT/bin/conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/r
     $CONDA_ROOT/bin/conda install -y mamba -c conda-forge -n base
     MAMBA_ROOT_PREFIX=$CONDA_ROOT $CONDA_ROOT/bin/mamba create -y -n hsgym python=3.8 -c conda-forge --override-channels
   fi
